e6b2b4c49005e2ef62e8dd92c2663cc11c3aff1f
Guzmán Facundo <guzmanfacu02@hotmail.com>
2025-12-04 12:35:09 -0300

    fix

--- workhub/frontend/src/app/services/team.service.ts
+++ workhub/frontend/src/app/services/team.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, inject } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Observable, catchError, throwError } from 'rxjs';
+import { Observable, catchError, throwError, of } from 'rxjs';
 import { Team, TeamMember } from '../interfaces/team';
 import { endpointFor } from './api-config';
 import { toFriendlyError } from './error.utils';
